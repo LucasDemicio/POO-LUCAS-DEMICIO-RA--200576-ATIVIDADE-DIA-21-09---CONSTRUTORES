b076320a5ec1635d457e4b0bd631d9800d2e88c8
LucasDemicio <69490166+LucasDemicio@users.noreply.github.com>
2020-10-30 15:42:40 -0300

    Add files via upload

---
+++ Exercício construtores - Lucas/Exercício construtores - Lucas/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exercício_construtores___Lucas
+{
+    public class Produto
+    {
+        
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        public double ValorTotalEmEstoque()
+        { return Preco* Quantidade; }
+        public void AdicionarProduto(int quantidade)
+        { Quantidade += quantidade; }
+        public void RemoverProdutos(int quantidade)
+        { Quantidade = Quantidade - quantidade; }
+        public override string ToString()
+        {
+            return Nome + ", R$ "
+                   + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                   + ", "
+                   + Quantidade
+                   + " unidades, Total: R$ "
+                   + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+            //fiz o código, mas ele não roda.
